64030decb33d7ee2c4e3d6ac1b1512ab31ddcaad
joyel24 <joel.martin.2104@gmail.com>
2024-05-16 20:07:01 +0200

    add wall + clean code

--- WhiteCar.gd
+++ WhiteCar.gd
@@ -7,10 +7,8 @@ func _physics_process(delta):
 	var direction = Input.get_axis("ui_left", "ui_right")
 	if direction:
 		self.rotation += direction / 10
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
 	
-	velocity = transform.x * Input.get_axis("ui_down", "ui_up") * SPEED
+	velocity = transform.x * Input.get_axis("ui_down", "ui_up") * SPEED #Move
 	
 	
 	move_and_slide()
