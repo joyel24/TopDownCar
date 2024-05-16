d4cf977c06c5c0087506a93b71c0ffa746d22052
joyel24 <joel.martin.2104@gmail.com>
2024-05-16 19:53:25 +0200

    camera follow+ugly background moves

--- WhiteCar.gd
+++ WhiteCar.gd
@@ -2,32 +2,15 @@ extends CharacterBody2D
 
 
 const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
-
-# Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-
 
 func _physics_process(delta):
-	# Add the gravity.
-	#if not is_on_floor():
-		#velocity.y += gravity * delta+
-	#var angle = 0
-	#angle += 1
-	#self.rotation +=1
-	pass
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("ui_left", "ui_right")
 	if direction:
 		self.rotation += direction / 10
-		#velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+	
+	velocity = transform.x * Input.get_axis("ui_down", "ui_up") * SPEED
+	
+	
 	move_and_slide()
