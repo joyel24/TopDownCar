885df09049ab5a3fc15fb5735742cca7307ee333
joel <joel.martin.2104@gmail.com>
2024-05-19 00:05:07 +0200

    add timer score

--- world.gd
+++ world.gd
@@ -1,9 +1,12 @@
 extends Node2D
 
+var score = 0
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	score = 0
 	$WhiteCar/CanvasLayer/checkpoints_label.set("theme_override_font_sizes/font_size", 60)
-	Global.rotate=0
+	Global.rotate=0.0
 	if Global.reset==true:
 		Global.reset=false
 		Global.checkpoint_total = Global.checkpoint_total / 2
@@ -12,8 +15,10 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	#print(score)
+	score += 1
 	#print(str(Global.checkpoint_total)+" "+str(Global.checkpoint_valid))
-	$WhiteCar/CanvasLayer/checkpoints_label.text = (str(Global.checkpoint_valid)+" / "+str(Global.checkpoint_total))
+	$WhiteCar/CanvasLayer/checkpoints_label.text = (str(Global.checkpoint_valid)+" / "+str(Global.checkpoint_total)+"\n"+str(score))
 	pass
 
 func _on_area_2d_mouse_entered():
